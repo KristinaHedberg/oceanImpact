7d87e6deb8d8da99a85fc1f46ff617a996b7b1f0
KristinaHedberg <khed4410@uni.sydney.edu.au>
2020-11-08 22:35:27 +0000

    add background image and ruin nav responsive

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const navSlide = () => {
-  const burger = document.querySelector ('.burger');
+  const menu = document.querySelector ('.menu');
   const nav = document.querySelector ('.nav-links');
   const navLinks = document.querySelectorAll ('.nav-links li');
 
-  burger.addEventListener('click',() => {
+  menu.addEventListener('click',() => {
     nav.classList.toggle('nav-active');
 
     navLinks.forEach((link,index) => {
@@ -13,8 +13,8 @@ const navSlide = () => {
         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 6 + 0.5}s`;
       }
     });
-    //burger animation 
-    burger.classList.toggle('toggle');
+    //menu animation 
+    menu.classList.toggle('toggle');
   });
 
 }
