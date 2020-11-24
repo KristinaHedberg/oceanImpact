3f47ad4d8e051c2ad842d8777ff08af7d5834b1e
KristinaHedberg <khed4410@uni.sydney.edu.au>
2020-11-24 05:25:12 +0000

    working image and text responsive to iphone x

--- script.js
+++ script.js
@@ -19,3 +19,16 @@ const navSlide = () => {
 
 }
 navSlide(); 
+
+window.onscroll = function() {myFunction()};
+
+var navBar = document.getElementById("navBar");
+var sticky = navBar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navBar.classList.add("sticky")
+  } else {
+    navBar.classList.remove("sticky");
+  }
+}
